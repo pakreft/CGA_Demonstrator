7903c0d2205d37dbeb2453cf53aa6ce60420300d
mehrlean <66731443+mehrlean@users.noreply.github.com>
2022-11-22 00:36:13 +0100

    declared Variables for constructor

--- src/setups/sunLight.js
+++ src/setups/sunLight.js
@@ -4,19 +4,22 @@ import {globals} from './globals.js';
 
 export const posSunStart = new THREE.Vector3(0, 80, 100);
 
+const sunIntensity = 0.7;
+const ambientIntensity = 0.8;
+const sunPenumbra = 1;
+const lightColor = 0xffffff;
+
+
 export default function setupSunLight() {
-  const ambientLight = new THREE.AmbientLight(0xffffff);
-  ambientLight.intensity = 0.8;
+  const ambientLight = new THREE.AmbientLight(lightColor);
+  ambientLight.intensity = ambientIntensity;
   globals.scene.add(ambientLight);
-
-  globals.sun = new THREE.PointLight(0xffffff);
+  globals.sun = new THREE.PointLight(lightColor);
+  globals.sun.intensity = sunIntensity;
+  globals.sun.penumbra = sunPenumbra;
   globals.sun.position.set(posSunStart.x, posSunStart.y, posSunStart.z);
-  globals.sun.intensity = 0.7;
-  globals.sun.penumbra = 1;
-
   globals.sun.castShadow = true;
   globals.sun.shadow.mapSize.set(2048, 2048);
-
   globals.scene.add(globals.sun);
   globals.scene.add(new THREE.CameraHelper(globals.sun.shadow.camera));
 }
