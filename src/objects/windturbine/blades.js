220c2989299d69623824b6c87c72dd147451adec
mehrlean <66731443+mehrlean@users.noreply.github.com>
2023-01-05 13:50:09 +0100

    Initial Rotation of the Blade applies to the local Rotation-Values Added Function to Change the Blade Angle.

--- src/objects/windturbine/blades.js
+++ src/objects/windturbine/blades.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import * as TWEEN from 'tween';
+import {globals} from "../../setups/globals.js";
 
 
 export default class Blades extends THREE.Mesh {
@@ -12,23 +13,53 @@ export default class Blades extends THREE.Mesh {
             const materialColor = 0xaada3a;
             const geometry = new THREE.BoxGeometry(width,height,depth,16,16,16);
             const material = new THREE.MeshLambertMaterial({color: materialColor});
-            geometry.translate(geometry.parameters.width/2,geometry.parameters.height/2+2.8, geometry.parameters.depth/2);
-            geometry.rotateX(THREE.MathUtils.degToRad(rot.x));
-            geometry.rotateY(THREE.MathUtils.degToRad(rot.y));
-            geometry.rotateZ(THREE.MathUtils.degToRad(rot.z));
 
+            geometry.translate(geometry.parameters.width/2,geometry.parameters.height/2+2.8, geometry.parameters.depth/2);
 
             super(geometry, material);
             this.castShadow = true;
             this.receiveShadow = true;
             this.animationRunning = false;
 
+            window.console.log(this);
 
 
-          this.rotateX(THREE.MathUtils.degToRad(rot.x));
-          this.rotateY(THREE.MathUtils.degToRad(rot.y));
-          this.rotateZ(THREE.MathUtils.degToRad(rot.z));
+
+
+         //   geometry.rotation.set(rot.x,rot.y,rot.z);
+            //this.setRotationFromQuaternion(new THREE.Quaternion(rot.x,rot.y,rot.z),);
+            this.rotateOnWorldAxis(new THREE.Vector3(1,0,0),THREE.MathUtils.degToRad(rot.x));
+            this.rotateOnWorldAxis(new THREE.Vector3(0,1,0),THREE.MathUtils.degToRad(rot.y));
+            this.rotateOnWorldAxis(new THREE.Vector3(0,0,1),THREE.MathUtils.degToRad(rot.z));
+
+            this.add( new THREE.AxesHelper( 50 ));
+
+            //globals.scene.add( axesHelper );
+
+
+
+
+
 
         }
+
+        changeBladeAngle(val){
+
+
+            if (val > this.rotation.y){
+                this.rotateOnAxis(new THREE.Vector3(0,1,0),val-this.rotation.y);
+            }
+
+            else if (val < this.rotation.y){
+                this.rotateOnAxis(new THREE.Vector3(0,1,0),val-this.rotation.y);
+            }
+            else{return;
+            }
+
+            window.console.log(this.rotation.y);
+
+
+    }
+
 
 }
