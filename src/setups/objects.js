409129d95982aac34422359b3dc596613ccfd25d
mehrlean <66731443+mehrlean@users.noreply.github.com>
2023-01-05 17:51:34 +0100

    Added GLTF Loader, Added Blender Model of WindTurbine

--- src/setups/objects.js
+++ src/setups/objects.js
@@ -4,13 +4,40 @@ import {globals} from './globals.js';
 import Ground from '../objects/ground.js';
 import WindTurbine from '../objects/windturbine/windTurbine.js';
 import Blades from '../objects/windturbine/blades.js';
+import { GLTFLoader } from '../../lib/three.js-r145/examples/jsm/loaders/GLTFLoader.js';
+
 
 
 
 export default function addTestObj() {
-  let windTurbine = new WindTurbine(new THREE.Vector3(20,0, 12),new THREE.Vector3(0,23,0));
+  let windTurbine = new WindTurbine(new THREE.Vector3(20,0,0),new THREE.Vector3(0,0,0));
   globals.scene.add(windTurbine);
   globals.windTurbine = windTurbine;
+
+
+
+  let loader = new GLTFLoader();
+// Load the glTF file
+  loader.load("src/assets/Models/windrad_export.gltf", function (gltf) {
+
+    const WindTurbineGLTF =  gltf.scene;
+    // Add the model to the scene
+    WindTurbineGLTF.position.set(-20,0,0);
+
+
+    globals.scene.add(WindTurbineGLTF);
+  window.console.log(WindTurbineGLTF);
+
+
+    WindTurbineGLTF.traverse((child) => {
+      if (child.name === 'Head') {
+        window.console.log('Found Head:', child);
+
+
+      }
+    });
+  });
+
 
 
   globals.scene.add(new Ground());
