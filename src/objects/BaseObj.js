7ba756472c8b5e20ee626d0e6f5fdd80607adec1
Pawel Kreft <pkreft@outlook.de>
2023-01-30 17:05:20 +0100

    Created BaseObj.js

---
+++ src/objects/BaseObj.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+/**
+ * Base class for all meshes/groups. Only inherit from this.
+ * @extends Group
+ * @abstract
+ */
+export default class BaseObj extends THREE.Group {
+
+  /**
+   * @param {{x: number, y: number, z: number}} pos - Objects position in world space.
+   * @param {{x: number, y: number, z: number}} rot - Objects rotation in world space in degrees.
+   * @param {Mesh[]|Group[]} objects - The meshes/groups to add to this object.
+   */
+  constructor(pos, rot, objects) {
+    super();
+
+    // Add meshes/groups to this group
+    for(let i = 0; i <= objects.length; i++) {
+      this.add(objects[i]);
+    }
+
+    // Set position and rotation
+    this.position.set(pos.x, pos.y, pos.z);
+    this.rotation.set(
+        THREE.MathUtils.degToRad(rot.x),
+        THREE.MathUtils.degToRad(rot.y),
+        THREE.MathUtils.degToRad(rot.z)
+    );
+
+    // Add local axes for debugging
+    this.add(new THREE.AxesHelper(50));
+  }
+
+}
